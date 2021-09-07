eeb5cbde65303630728ff42043cc53ef1d1f60a3
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-09-07 19:30:24 -0400

    Lab 3 script added

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -3,6 +3,15 @@
     }
 
 $IP = getIP
+$USER = $env:USERNAME
+$HOSTNAME = $env:COMPUTERNAME
+$PVERSION = (get-host).Version 
+$DAYOFWEEK = (get-date).DayOfWeek
+$MONTH= (get-date).Month
+$DAY= (get-date).Day
+$YEAR= (get-date).Year
 
-Write-Host("This machine's IP is $IP")
-Write-Host("This machine's IP is {0}" -f $IP)
+
+$BODY = "This machine's IP is $IP. User is $USER. Hostname is  $HOSTNAME. Powershell version is $PVERSION. Today's Date is $DAYOFWEEK, $MONTH/$DAY/$YEAR"
+
+Send-MailMessage -To "sekhonss@mail.uc.edu" -From "sukhbirsekhon3939@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
