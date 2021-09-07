17c6b24c7322f87a089a12ceba23097df9f643e8
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-09-07 19:32:07 -0400

    added comments

--- powershell/sysinfo.ps1
+++ powershell/sysinfo.ps1
@@ -1,7 +1,9 @@
-﻿function getIP {
+﻿# Function to get ip address
+function getIP {
     (Get-NetIPAddress).ipv4address | Select-string "192.168*"
     }
 
+# All the required variables for Body message
 $IP = getIP
 $USER = $env:USERNAME
 $HOSTNAME = $env:COMPUTERNAME
@@ -11,7 +13,8 @@ $MONTH= (get-date).Month
 $DAY= (get-date).Day
 $YEAR= (get-date).Year
 
-
+# Creating a body for email
 $BODY = "This machine's IP is $IP. User is $USER. Hostname is  $HOSTNAME. Powershell version is $PVERSION. Today's Date is $DAYOFWEEK, $MONTH/$DAY/$YEAR"
 
+#Sending email message using SMTP server
 Send-MailMessage -To "sekhonss@mail.uc.edu" -From "sukhbirsekhon3939@gmail.com" -Subject "IT3038C Windows SysInfo" -Body $BODY -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential) 
