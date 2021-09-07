642666bbe6b32cfe3c04415d24d8b9923aeb0290
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-09-07 18:55:46 -0400

    added sysinfo powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,8 @@
+﻿function getIP {
+    (Get-NetIPAddress).ipv4address | Select-string "192.168*"
+    }
+
+$IP = getIP
+
+Write-Host("This machine's IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
