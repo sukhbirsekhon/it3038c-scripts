9e63ecd7c884245af7a9e9baad1b7e598269cee0
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-09-21 18:49:02 -0400

    added myAge logic if-else

--- python/nameage.py
+++ python/nameage.py
@@ -8,6 +8,20 @@ myName = input()
 print('Hello, ' + myName + '. That is a good name. How old are you?')
 
 myAge = int(input())
+
+# determine message based on age
+if myAge < 13:
+    print("Learning young, that's good!")
+elif myAge == 13:
+    print("You're teenager now...")
+elif myAge > 13 and myAge < 30:
+    print("Still young, still learning...")
+elif myAge >= 30 and myAge < 65:
+    print("Now you're adulting")
+else: 
+    print("... you've lived a long time!")
+
+time.sleep(2)
 programAge = int(time.time() - start_time)
 
 print("%s? That's funny, I am only %s seconds old." % (myAge, programAge))
