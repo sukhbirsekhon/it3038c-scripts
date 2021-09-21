b164e385e23fdec5e5b287f2ef63fdf4def4e584
sukhbirsekhon <sekhonss@mail.uc.edu>
2021-09-21 18:43:25 -0400

    Added a nameage script

---
+++ python/nameage.py
@@ -0,0 +1,18 @@
+import time
+start_time = time.time()
+
+print('What is your name?')
+
+myName = input()
+
+print('Hello, ' + myName + '. That is a good name. How old are you?')
+
+myAge = int(input())
+programAge = int(time.time() - start_time)
+
+print("%s? That's funny, I am only %s seconds old." % (myAge, programAge))
+print("I wish I was %s years old" % (myAge * 2))
+
+time.sleep(1)
+print("I'm tired. I go to sleep now.")
+
